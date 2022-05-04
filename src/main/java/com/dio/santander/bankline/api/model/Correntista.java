b746e93c1775cf4b7321fc1ed74e723a3c5a80d9
lucascostadev <lucascostadev@gmail.com>
2022-05-04 20:07:07 -0300

    V1.4 Criação das Classes de Modelo (Model)

---
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -0,0 +1,52 @@
+package com.dio.santander.bankline.api.model;
+
+
+/*
+================Informações sobre as Classes Models===================
+  Nas classes de modelo precisamos gerar nossos métodos Geters and Seters para acessos.
+  Podemos realizar isso gerando manualmente ou
+  Usar uma dependência chamada Lombook que gera esses geters and seters em tempo de execução.
+=======================================================================
+
+ */
+public class Correntista {
+
+    private Integer id;
+    private String cpf;
+    private String nome;
+
+    /*Informando que Correntista possui uma Conta->Classe*/
+    private Conta conta;
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
