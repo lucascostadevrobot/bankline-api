e2ef80ab20832e3128147c8a7cbf862441d7cfbf
lucascostadeviff <lucascostadev@gmail.com>
2022-05-04 16:05:11 -0300

    V1.2 Alteração na Classe principal

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -3,6 +3,8 @@ package com.dio.santander.bankline.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*Desenvolvimento de uma API de consumo para aplicação Web e Mobile*/
+
 @SpringBootApplication
 public class BanklineApiApplication {
 
