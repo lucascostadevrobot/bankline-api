ab0ca115efcc9ae7a43b1c8496f21b6bd3571f72
lucascostadeviff <lucascostadev@gmail.com>
2022-05-05 13:29:31 -0300

    V1.8 Finalizando Mapeamento das classes Model
    Finalizando o mapeamento das classes de entidade dentro do pacote Model.

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -1,14 +1,22 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tab_movimentacao")
 public class Movimentacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "data_hora")
     private LocalDateTime dataHora;
+    
     private String descricao;
     private Double valor;
 
     /*Informo a minha classe Movimentacao qual o tipo ela será MovimentacaoTipo->Classe*/
+    @Enumerated(EnumType.STRING)
     private  MovimentacaoTipo tipo;
 
     public Integer getId() {
