c0d56b653a259481c740769ad90135ab9b9c2436
lucascostadev <lucascostadev@gmail.com>
2022-05-09 14:11:32 -0300

    V2.19 Criando Classe MovimentacaoController
    Criando e configurando a classe MovimentacaoController

---
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -0,0 +1,34 @@
+package com.dio.santander.bankline.api.controller;
+
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
+import com.dio.santander.bankline.api.model.Movimentacao;
+import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/movimentacoes")
+public class MovimentacaoController {
+
+    /*Criando Metodo para listar correntistas*/
+    @Autowired
+    private MovimentacaoRepository repository;
+
+    @Autowired
+    private CorrentistaService service;
+
+    @GetMapping /*Dizemos que nosso metodo é uma requisição do endpoint ("/correntistas")*/
+    public List<Movimentacao> findAll(){
+        return repository.findAll();
+
+    }
+
+    @PostMapping
+    public void save(@RequestBody NovoCorrentista correntista) {
+        service.save(correntista);
+    }
+}
---
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -0,0 +1,27 @@
+package com.dio.santander.bankline.api.service;
+
+import com.dio.santander.bankline.api.dto.NovaMovimentacao;
+import com.dio.santander.bankline.api.model.Movimentacao;
+import com.dio.santander.bankline.api.model.MovimentacaoTipo;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class MovimentacaoService {
+public void save (NovaMovimentacao novaMovimentacao){
+
+    Movimentacao movimentacao = new Movimentacao();
+
+    Double  valor = novaMovimentacao.getValor();
+    if(novaMovimentacao.getTipo() == MovimentacaoTipo.DESPESA)
+        valor = valor * -1;
+
+    movimentacao.setDataHora(LocalDateTime.now());
+    movimentacao.setDescricao(novaMovimentacao.getDescricao());
+    movimentacao.setIdConta(novaMovimentacao.getIdConta());
+    movimentacao.setTipo(novaMovimentacao.getTipo());
+    movimentacao.setValor(valor); //Valor passado do calculo de acordo com a receita da regra acima
+}
+
+}
