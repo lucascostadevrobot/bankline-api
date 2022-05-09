c4d9d5403c9b14bfd8552218e82560825d4199df
lucascostadev <lucascostadev@gmail.com>
2022-05-09 13:17:49 -0300

    v1.16 Configurando CorrentistaController
    Configurando e Instanciando nossa Classe CorrentistaController

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -1,11 +1,11 @@
 package com.dio.santander.bankline.api.controller;
 
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +18,17 @@ public class CorrentistaController {
     @Autowired
     private CorrentistaRepository repository;
 
+    @Autowired
+    private CorrentistaService service;
 
     @GetMapping /*Dizemos que nosso metodo é uma requisição do endpoint ("/correntistas")*/
     public List<Correntista> findAll(){
         return repository.findAll();
 
+    }
+
+    @PostMapping
+    public void save(@RequestBody NovoCorrentista correntista) {
+        service.save(correntista);
     }
 }
---
+++ src/main/java/com/dio/santander/bankline/api/service/CorrentistaService.java
@@ -0,0 +1,29 @@
+package com.dio.santander.bankline.api.service;
+
+
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
+import com.dio.santander.bankline.api.model.Conta;
+import com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class CorrentistaService {
+    @Autowired
+    private CorrentistaRepository repository;
+
+    public void  save (NovoCorrentista novoCorrentista){
+        Correntista correntista = new Correntista();
+        correntista.setCpf(novoCorrentista.getCpf());
+        correntista.setNome(novoCorrentista.getNome());
+
+        Conta conta = new Conta();
+        conta.setSaldo(0.0); /*Todo novo correntista tera o inicio da conta 0.0*/
+        conta.setNumero(new Date().getTime()); /*Digo que vai gerar numero da nossa conta*/
+
+        correntista.setConta(correntista);
+    }
+}
